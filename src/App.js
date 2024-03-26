d9d42ee8596d9ac6bfc9c5691aace31f168712d6
dselokela023 <dselokela023@student.wethinkco.de>
2024-03-26 21:08:43 +0200

    almost done

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ function App() {
   //state variables
   const [tokens, setTokens] = useState([]);
   const [address, setAddress] = useState("");
+  const [eth,setEth] = useState();
  
   //handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
     setAddress(address);
+    fetchEth();
     fetchTokens()
       .then((data) => {
         setTokens(data.assets);
@@ -30,19 +32,30 @@ function App() {
         wallet: address,
         // contracts: []
         contracts: [
-          "0xdAC17F958D2ee523a2206206994597C13D831ec7",//USdT
+          "0xdAC17F958D2ee523a2206206994597C13D831ec7",//USDT
           "0xB8c77482e45F1F44dE1745F52C74426C631bDD52",//BNB
           "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",//USDC
           "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84", //stETH
           "0x582d872A1B094FC48F5DE31D3B73F2D9bE47def1",//TONCOIN
-          "",
-          "",
+          "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE", //SHIB
+          "0x514910771AF9Ca656af840dff83E8264EcF986CA", //Link
 
 
         ]
       });
       return tokens;
     };
+
+    const fetchEth = async () => {
+      const provider = new ethers.JsonRpcProvider(
+        "https://greatest-wiser-tab.quiknode.pro/5547b455e95e1f15626cfb9e8045daa549389c9c/"
+        );
+      //fetch ETH balance
+      const eth = await provider.send("eth_getBalance", [address, "Latest"]);
+      
+      setEth(eth);
+    };
+
     return(
       <div className="h-screen w-screen justify-center space-x-3 ml-5">
       <div className="flex space-x-3 w-screen h-14 ml-2 mt-10">
@@ -64,6 +77,12 @@ function App() {
             Show me the tokens</button>
         </form>
       </div>
+      {eth &&(
+        <div className='relative mt-10 text-gray-900'>
+          ETH balance:&nbsp; <strong>{ethers.formatEther(eth)}</strong>
+          </div>
+      )}
+
       {tokens.length > 0 && (
         <div className='relative overflow-x-auto justify-center space-x-3 w-6/12 h-140 mt-10 mb-10'>
           <h1 className='text-3xl'>Tokens</h1>
